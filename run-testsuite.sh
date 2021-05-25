1eca6384f99478dc3019b525d55bc2b3b28a1478
Katia Aresti <karesti@redhat.com>
2021-05-25 14:48:05 +0200

    SASL Authentication

--- run-testsuite.sh
+++ run-testsuite.sh
@@ -3,4 +3,5 @@
 # Stop if any errors found
 set -e
 
-node --trace-deprecation node_modules/jasmine-node/lib/jasmine-node/cli.js spec/* --captureExceptions
+node --trace-deprecation node_modules/jasmine-node/lib/jasmine-node/cli.js spec/infinispan_auth_spec.js --captureExceptions
+#node --trace-deprecation node_modules/jasmine-node/lib/jasmine-node/cli.js spec/* --captureExceptions
